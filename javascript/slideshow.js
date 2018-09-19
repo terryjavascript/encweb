d47014081141f371a6b1c5d8aa9a94933ec5cff4
terry.javascript <terry.javascript@gmail.com>
2018-09-18 18:22:41 -0700

    first push

---
+++ javascript/slideshow.js
@@ -0,0 +1,19 @@
+// image slideshow
+
+
+
+var imageArray = ["assets/images/ss1.png","assets/images/ss3.png","assets/images/ss4.png","assets/images/ss5.png", "assets/images/ss2.png"];
+
+var imageIndex = 0;	
+
+function changeImage(){
+	slide.setAttribute("src", imageArray [imageIndex]);
+	imageIndex++;
+	if (imageIndex>=imageArray.length) {
+		imageIndex = 0;
+	}
+};
+
+console.log(changeImage);
+
+var intervalHandle = setInterval(changeImage,5000);
