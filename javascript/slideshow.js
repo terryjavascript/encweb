d44154d898575ced4d85f68ed199466884669caf
terry.javascript <terry.javascript@gmail.com>
2018-10-02 16:33:39 -0700

    update

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,7 +1,7 @@
 // image slideshow
 
-var imageArray = ["assets/images/ss1.png","assets/images/ss2.png","assets/images/ss3.png","assets/images/ss4.png", "assets/images/ss5.png", "assets/images/ss0.png"];
-
+var imageArray = ["assets/images/airplane.jpg","assets/images/airplane2.jpg","assets/images/airplane3.jpg","assets/images/airplane4.jpg","assets/images/ss0.png"];
+//"assets/images/ss1.png","assets/images/ss2.png","assets/images/ss3.png","assets/images/ss4.png", "assets/images/ss5.png", "assets/images/ss0.png"//
 var imageIndex = 0;	
 
 function changeImage(){
